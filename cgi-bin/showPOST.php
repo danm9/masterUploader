71836dcb2879f17edf9360a43e6223ae684fa82d
Brockman Lab (ERIC) <B_Team_iMac@brockman-srv.mbb.sfu.ca>
2014-08-28 16:49:29 -0700

    touch up

--- cgi-bin/showPOST.php
+++ cgi-bin/showPOST.php
@@ -1,6 +1,13 @@
 <?php
 
-print_r($_POST);
+define('db_username', 'root');
+define('db_password', 'nef');
+define('db_host', '127.0.0.1');
+define('db_name', 'daniel_test');
+
+$conn = new mysqli(db_host, db_username, db_password, db_name);
+
+//print_r($_POST);
 
 // Split textarea data by newline, then by tabs
 $textAreaData = preg_split('/\r\n|\n|\r/', $_POST["textArea"]);
@@ -25,10 +32,16 @@ foreach($textAreaData as $line) {
 	$values = array();
 	foreach(array_values($line) as $i => $item) {
 		$fields[$i] = $_POST['data_'.$i];
-		$values[$i] = $item;
+		$values[$i] = '\'' . $item . '\'';
 	}
 	$fields = implode(",", $fields);
 	$values = implode(",", $values);
 	$query .= "$fields) VALUES ($values)";
-	echo $query;
+	echo "Executing: $query\n";
+	if ($conn->query($query)) {
+		echo "SUCCESS!";
+	}
+	else {
+		echo $conn->error;
+	}
 }
